3b27279c61bfe259a133d30c83ddfb884a26965e
Divay Prakash <divayprakash3@gmail.com>
2017-07-11 22:48:15 +0530

    WIP
    Add medication tab content, add JS to read values from modal submit, update CSS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
   $('.chips').material_chip();
   $('.datepicker').pickadate({
+    format: 'dd/mm/yyyy',
     selectMonths: true
   });
+  $('.datepicker-year').pickadate({
+    format: 'dd/mm/yyyy',
+    selectMonths: true,
+    selectYears: 7
+  });
+  $('#medication-add').modal();
 });
+
+var medicationCount = 0;
+var medicationOne;
+function medicationAdd() {
+  var name = $('#medication-name').val();
+  medicationOne = name;
+  var manufacturer = $('#manufacturer').val();
+  var batch = $('#batch').val();
+  var expiry = $('#expiry').pickadate().pickadate('picker').get();
+  var dose = $('#dose').val();
+  var route = $("input[type='radio'][name='route']:checked").val();
+  var frequency = $('#freq').val();
+  var therapyStart = $('#therapy-start').pickadate().pickadate('picker').get();
+  var therapyEnd = $('#therapy-end').pickadate().pickadate('picker').get();
+  var duration = $('#duration').val();
+  var reason = $('#reason').val();
+}

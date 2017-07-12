df61b0d8dd5dd379740fd57dbcd1058f60661794
Divay Prakash <divayprakash3@gmail.com>
2017-07-13 00:00:57 +0530

    Update JS for medication modal content added in e2a776f

--- js/main.js
+++ js/main.js
@@ -10,13 +10,31 @@ $(document).ready(function(){
     selectYears: 7
   });
   $('#medication-add').modal();
+  enableSpecFieldOnRadio('stop','#stop-reduced');
+  enableSpecFieldOnRadio('reintro','#reintro-reduced');
 });
 
+function enableSpecFieldOnRadio(radioId, fieldSelector) {
+  $('input[type=radio][name=' + radioId + ']').on('change', function() {
+    switch($(this).val()) {
+      case 'yes':
+        $(field).prop('disabled', false);
+        break;
+      case 'no':
+      case 'unknown':
+      case 'na':
+        $(fieldSelector).val("");
+        $(fieldSelector).removeClass('valid');
+        $(fieldSelector).removeClass('invalid');
+        $(fieldSelector).prop('disabled', true);
+        break;
+    }
+  });
+}
+
 var medicationCount = 0;
-var medicationOne;
 function medicationAdd() {
   var name = $('#medication-name').val();
-  medicationOne = name;
   var manufacturer = $('#manufacturer').val();
   var batch = $('#batch').val();
   var expiry = $('#expiry').pickadate().pickadate('picker').get();
@@ -27,4 +45,10 @@ function medicationAdd() {
   var therapyEnd = $('#therapy-end').pickadate().pickadate('picker').get();
   var duration = $('#duration').val();
   var reason = $('#reason').val();
+  var stop = $("input[type='radio'][name='stop']:checked").val();
+  var stopReduced = 0;
+  if (stop == "yes") stopReduced = $('#stop-reduced').val();
+  var reintro = $("input[type='radio'][name='reintro']:checked").val();
+  var reintroReduced = 0;
+  if (reintro == "yes")  reintroReduced = $('reintro-reduced').val();
 }

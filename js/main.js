092825a2bdce8151167d243e7d93210c09b2ded9
Divay Prakash <divayprakash3@gmail.com>
2017-09-29 17:26:03 +0530

    WIP: Style medication div added on form submit

--- js/main.js
+++ js/main.js
@@ -71,7 +71,7 @@ function medicationAdd() {
   formReset();
   $('#medication-add').modal('close');
   Materialize.toast('Medication added!', 4000, 'rounded');
-  var divToAdd = "<div class='card blue lighten-4'><div class='card-content'><span class='card-title'>"+ name +"</span><p>"+ manufacturer + " / " + expiry + "</p></div><div class='card-action'><button class='waves-effect waves-light btn'><i class='material-icons right'>mode_edit</i>Edit</button><button class='waves-effect waves-light btn'><i class='material-icons right'>delete</i>Delete</button></div></div>";
+  var divToAdd = "<div class='card blue lighten-4'><div class='card-content'><span class='card-title'>"+ name +"</span><p>"+ manufacturer + "<br>" + expiry + "</p></div><div class='card-action'><button class='waves-effect waves-light btn'><i class='material-icons right'>mode_edit</i>Edit</button><button class='waves-effect waves-light btn'><i class='material-icons right'>delete</i>Delete</button></div></div>";
   $('#insert').before(divToAdd);
 }
 

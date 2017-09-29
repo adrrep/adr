0fcd1463c255f1cfc71518b3c52f5e5cfd827fa0
Divay Prakash <divayprakash3@gmail.com>
2017-09-29 16:22:45 +0530

    WIP: Add medication div on form submit

--- js/main.js
+++ js/main.js
@@ -69,4 +69,6 @@ function medicationAdd() {
   var reintroReduced = 0;
   if (reintro == "yes")  reintroReduced = $('reintro-reduced').val();
   $('#medication-add').modal('close');
+  var divToAdd = "<div class='card blue lighten-4'><div class='card-content'><span class='card-title'>"+ name +"</span><p>"+ manufacturer + " / " + expiry + "</p></div><div class='card-action'><button class='waves-effect waves-light btn'><i class='material-icons right'>mode_edit</i>Edit</button><button class='waves-effect waves-light btn'><i class='material-icons right'>delete</i>Delete</button></div></div>";
+  $("#insert").before(divToAdd);
 }

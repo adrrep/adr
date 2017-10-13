13195f6c565e8fe02bec571d37b87653285c030e
Divay Prakash <divayprakash3@gmail.com>
2017-10-13 23:18:20 +0530

    Hotifx: Remove highly error prone datauri test
    This was causing issues on normal refresh of page after initial load as it is async and fails due to some caching issue IDK

--- js/compat.js
+++ js/compat.js
@@ -3,16 +3,12 @@
 * Copyright (C) 2017 Divay Prakash
 * GNU Affero General Public License 3.0 (https://github.com/divayprakash/adr/blob/master/LICENSE)
 */
-var dataUriTest;
-Modernizr.on('datauri', function(result) {
-  dataUriTest = result.valueOf() && result.over32kb;
-  var inputTest = Modernizr.input.max && Modernizr.input.min && Modernizr.input.placeholder && Modernizr.input.step;
-  var inputTypesTest = Modernizr.inputtypes.date && Modernizr.inputtypes.email && Modernizr.inputtypes.number && Modernizr.inputtypes.range && Modernizr.inputtypes.tel;
-  var mediaQueriesTest = Modernizr.mediaqueries;
-  var placeholderTest = Modernizr.placeholder;
-  var totalTest = dataUriTest && inputTest && inputTypesTest && mediaQueriesTest && placeholderTest;
-  if (!totalTest) {
-    document.getElementById('main-div').style.display = 'none';
-    document.getElementById('warning').style.display = 'block';
-  }
-});
+var inputTest = Modernizr.input.max && Modernizr.input.min && Modernizr.input.placeholder && Modernizr.input.step;
+var inputTypesTest = Modernizr.inputtypes.date && Modernizr.inputtypes.email && Modernizr.inputtypes.number && Modernizr.inputtypes.range && Modernizr.inputtypes.tel;
+var mediaQueriesTest = Modernizr.mediaqueries;
+var placeholderTest = Modernizr.placeholder;
+var totalTest = inputTest && inputTypesTest && mediaQueriesTest && placeholderTest;
+if (!totalTest) {
+  document.getElementById('main-div').style.display = 'none';
+  document.getElementById('warning').style.display = 'block';
+}

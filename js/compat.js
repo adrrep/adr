ae9cc09081c4d197e6dbd9f76c0b6df31738087f
Divay Prakash <divayprakash3@gmail.com>
2017-10-14 03:43:33 +0530

    Fix license links in comments

--- js/compat.js
+++ js/compat.js
@@ -1,7 +1,7 @@
 /*
 * ADR Reporting
 * Copyright (C) 2017 Divay Prakash
-* GNU Affero General Public License 3.0 (https://github.com/divayprakash/adr/blob/master/LICENSE)
+* GNU Affero General Public License 3.0 (https://github.com/adrrep/adr/blob/master/LICENSE)
 */
 var inputTest = Modernizr.input.max && Modernizr.input.min && Modernizr.input.placeholder && Modernizr.input.step;
 var inputTypesTest = Modernizr.inputtypes.date && Modernizr.inputtypes.email && Modernizr.inputtypes.number && Modernizr.inputtypes.range && Modernizr.inputtypes.tel;
